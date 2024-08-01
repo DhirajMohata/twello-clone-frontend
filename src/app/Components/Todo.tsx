aeb0c734c21d87bc1eecd71347e1d01f16de07e9
DhirajMohata <dhirajmohata86@gmail.com>
2024-08-01 12:23:46 +0530

    Date change

--- src/app/Components/Todo.tsx
+++ src/app/Components/Todo.tsx
@@ -73,7 +73,7 @@ const Todo = ({ heading, tasks }: Taskboard) => {
 
                       {task.priority}
                     </p>
-                    {task.description && (
+                    {task.date && (
                     <p className="text-gray-500 inline-flex gap-2 mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
